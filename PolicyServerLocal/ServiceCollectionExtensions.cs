d194491d27ae233b980c4d46fe943378e5d5b305
Dominick Baier <dbaier@leastprivilege.com>
2017-11-26 15:40:00 +0100

    add more boilerplate

---
+++ PolicyServerLocal/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PolicyServerLocal
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddPolicyServerClient(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<Policy>(configuration);
+
+            return services;
+        }
+    }
+}
