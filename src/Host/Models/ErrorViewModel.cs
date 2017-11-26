c44bba7bb4878b975a407ad4b9133a209705e3a2
Dominick Baier <dbaier@leastprivilege.com>
2017-11-26 13:31:51 +0100

    initial project structure

---
+++ src/Host/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Host.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
